6d9c1b612bc9c2200d8895ce0a879342acd74a31
Caroline Magnain <cmagnain@mgh.harvard.edu>
2020-07-09 15:13:31 -0400

    Improve scripts

--- Process_PrairieData/Func_ExtractParameters.m
+++ Process_PrairieData/Func_ExtractParameters.m
@@ -1,19 +1,26 @@
 function Parameters = Func_ExtractParameters(xml_file,path)
 
+% This function will extract the relevant parameters of the experiment from
+% the .xml file saved by the software PrairieView version ??
+% by Caroline Magnain
+% Last Update 7/7/2020
+
 foldername = xml_file(1:end-4);
 c=textread([path '/' foldername '.xml'],'%s','delimiter','\n');
 
 Key   = 'value="';
-Parameters.FolderPath = [path];
-Parameters.FolderName = [foldername];
+Parameters.FolderPath = path;
+Parameters.FolderName = foldername;
 %% Laser Parameters
 
-ll = find(~cellfun(@isempty,strfind(c,'laserWavelength')));
+% Wavelength
+ll = find(contains(c,'laserWavelength'));
 Str = c{ll+1};
 Index = strfind(Str, Key);
 Parameters.LaserWavelength = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
 
-ll = find(~cellfun(@isempty,strfind(c,'laserPower')));
+% Power
+ll = find(contains(c,'laserPower'));
 Str = c{ll+1};
 Index = strfind(Str, Key);
 Parameters.LaserPower = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
@@ -22,20 +29,20 @@ Parameters.LaserPower = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
 
 %% PMT Gain
 
-ll = find(~cellfun(@isempty,strfind(c,'pmtGain')));
-%Channel 1
+ll = find(contains(c,'pmtGain'));
+% Channel 1
 Str = c{ll+1};
 Index = strfind(Str, Key);
 Parameters.PMT_Gain(1) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
-%Channel 2
+% Channel 2
 Str = c{ll+2};
 Index = strfind(Str, Key);
 Parameters.PMT_Gain(2) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
-%Channel 3
+% Channel 3
 Str = c{ll+3};
 Index = strfind(Str, Key);
 Parameters.PMT_Gain(3) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
-%Channel 4
+% Channel 4
 Str = c{ll+4};
 Index = strfind(Str, Key);
 Parameters.PMT_Gain(4) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
@@ -43,26 +50,30 @@ Parameters.PMT_Gain(4) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
 
 %% Imaging Parameters
 
-ll = find(~cellfun(@isempty,strfind(c,'objectiveLensMag')));
+% Objective
+ll = contains(c,'objectiveLensMag');
 Str = c{ll};
 Index = strfind(Str, Key);
 Parameters.Objective = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
 
-ll = find(~cellfun(@isempty,strfind(c,'opticalZoom')));
+% Optical Zoom
+ll = find(contains(c,'opticalZoom'));
 Str = c{ll};
 Index = strfind(Str, Key);
 Parameters.OpticalZoom = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
 
-ll = find(~cellfun(@isempty,strfind(c,'pixelsPerLine')));
+% Number of pixels per images
+ll = find(contains(c,'pixelsPerLine'));
 Str = c{ll};
 Index = strfind(Str, Key);
 Parameters.FOVpixels(1) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
-ll = find(~cellfun(@isempty,strfind(c,'linesPerFrame')));
+ll = find(contains(c,'linesPerFrame'));
 Str = c{ll};
 Index = strfind(Str, Key);
 Parameters.FOVpixels(2) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
 
-ll = find(~cellfun(@isempty,strfind(c,'micronsPerPixel')));
+% Pixel Size
+ll = find(contains(c,'micronsPerPixel'));
 Str = c{ll+1};
 Index = strfind(Str, Key);
 Parameters.PixelSize(1) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
@@ -73,14 +84,15 @@ Str = c{ll+3};
 Index = strfind(Str, Key);
 Parameters.PixelSize(3) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
 
-ll = find(~cellfun(@isempty,strfind(c,'framePeriod')));
+% Frame period
+ll = find(contains(c,'framePeriod'));
 Str = c{ll};
 Index = strfind(Str, Key);
 Parameters.FramePeriod = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
 
 %% Position
 
-ll = find(~cellfun(@isempty,strfind(c,'positionCurrent')));
+ll = find(contains(c,'positionCurrent'));
 Str = c{ll+2};
 Index = strfind(Str, Key);
 Parameters.Position(1) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
@@ -94,7 +106,7 @@ Parameters.Position(3) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
 %% Sequence
 
 Key   = 'type="';
-ll = find(~cellfun(@isempty,strfind(c,'Sequence')));
+ll = find(contains(c,'Sequence'));
 Str = c{ll};
 Index = strfind(Str, Key);
 Parameters.Sequence = sscanf(Str(Index(1) + length(Key):end), '%s', 1);
@@ -115,25 +127,25 @@ switch Parameters.Sequence
     case  'Single'
         Parameters.NbImages=1;
         for ii=ch
-            Parameters.Channel{ii}.name{1} = [foldername '_Cycle' sprintf('%05i',str2num(Parameters.Cycle)) '_Ch' sprintf('%i',ii)...
+            Parameters.Channel{ii}.name{1} = [foldername '_Cycle' sprintf('%05i',str2double(Parameters.Cycle)) '_Ch' sprintf('%i',ii)...
                 '_' sprintf('%06i',1) '.ome.tif'];
         end
     case  'TSeries'
-        ll = find(~cellfun(@isempty,strfind(c,'<Frame')));
+        ll = find(contains(c,'<Frame'));
         Parameters.NbImages=length(ll);
         for ii=ch
             for jj=1:length(ll)
-                Parameters.Channel{ii}.name{jj} = [foldername '_Cycle' sprintf('%05i',str2num(Parameters.Cycle)) '_Ch' sprintf('%i',ii)...
+                Parameters.Channel{ii}.name{jj} = [foldername '_Cycle' sprintf('%05i',str2double(Parameters.Cycle)) '_Ch' sprintf('%i',ii)...
                     '_' sprintf('%06i',jj) '.ome.tif'];
             end
         end
         
     case 'ZSeries'
-        mm = find(~cellfun(@isempty,strfind(c,'<Frame')));
+        mm = find(contains(c,'<Frame'));
         Parameters.NbImages=length(mm);
         for ii=ch
             for jj=1:length(mm)
-                Parameters.Channel{ii}.name{jj} = [foldername '_Cycle' sprintf('%05i',str2num(Parameters.Cycle)) '_Ch' sprintf('%i',ii)...
+                Parameters.Channel{ii}.name{jj} = [foldername '_Cycle' sprintf('%05i',str2double(Parameters.Cycle)) '_Ch' sprintf('%i',ii)...
                     '_' sprintf('%06i',jj) '.ome.tif'];
             end
         end
@@ -142,17 +154,17 @@ switch Parameters.Sequence
         for ii=1:length(mm)
             
             %laser power
-                ll = find(~cellfun(@isempty,strfind(c(mm(ii):end),'laserPower')));
-            if isempty(ll)==0
-                
+             ll = find(~cellfun(@isempty,strfind(c(mm(ii):end),'laserPower')));
+            if isempty(ll)==0                
                 Str = c{ll+1 + mm(ii)-1};
                 Index = strfind(Str, Key);
                 Parameters.ZSeries.LaserPower(ii) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
+            else
+                Parameters.ZSeries.LaserPower(ii)=NaN;                
             end
             
-            % PMT Gain
-            
-                ll = find(~cellfun(@isempty,strfind(c(mm(ii):end),'pmtGain')));
+            % PMT Gain           
+            ll = find(~cellfun(@isempty,strfind(c(mm(ii):end),'pmtGain')));
             if isempty(ll)==0
                 %Channel 1
                 Str = c{ll+1 + mm(ii)-1};
@@ -170,6 +182,8 @@ switch Parameters.Sequence
                 Str = c{ll+4 + mm(ii)-1};
                 Index = strfind(Str, Key);
                 Parameters.ZSeries.PMT_Gain(4,ii) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
+            else
+                Parameters.ZSeries.PMT_Gain(:,ii) = NaN;
             end
             
             % Position
@@ -184,10 +198,10 @@ switch Parameters.Sequence
                 Str = c{ll+8 + mm(ii)-1};
                 Index = strfind(Str, Key);
                 Parameters.ZSeries.Position(3,ii) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
+            else
+                Parameters.ZSeries.Position(:,ii) = NaN;
             end
         end
-        
-        
 end
 
 
